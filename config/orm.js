4c0e3f6ee402cc6e3c27fbe2575e98cfd0e696ec
Aleksandr Saiyan <anverch@gmail.com>
2020-06-10 23:42:52 -0500

    add Config Setup

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+// Select
+function selectAll(){
+
+}
+
+// Insert 
+function insertOne(){
+
+}
+
+// Update
+function updateOne(){
+
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Beef Burger");
+INSERT INTO burgers (burger_name) VALUES ("Turkey Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bison Burger");
