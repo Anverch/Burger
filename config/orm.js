898338b9677d48bdb304ed04b8430c2c503e7336
Aleksandr Saiyan <anverch@gmail.com>
2020-06-12 23:20:41 -0500

    add async and burger functions

--- config/orm.js
+++ config/orm.js
@@ -2,40 +2,28 @@
 var connection = require("../config/connection.js");
 
 // Select
-function selectAll(){
+async function selectAll(){
     const query = `SELECT * FROM burgers`;
-    connection.query(query, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        return result;
-      });
+    const result = await connection.query(query);
+    return result
 }
 
 // Insert 
-function insertOne(burgerName){
+async function insertOne(burgerName){
     const query = `INSERT INTO burgers (burger_name) VALUES ("${burgerName}");`;
-    connection.query(query, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        return result;
-      });
+    const result = await connection.query(query)
+    return result;
 }
 
 // Update
-function updateOne(id){
+async function updateOne(id){
     const query = `
         UPDATE burgers
         SET devoured = true
         WHERE id = ${id}
     `;
-    connection.query(query, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        return result;
-      });
+    const result = await connection.query(query)
+    return result;
 }
 
 const orm = {
--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,15 @@
 var orm = require("../config/orm");
 
 var burger = {
-    all: function(cb) {
-      orm.all("burgers", function(res) {
-        cb(res);
-      });
+    all: async ()=> {
+      return await orm.selectAll()
     },
-    // The variables cols and vals are arrays.
-    create: function(cols, vals, cb) {
-      orm.create("burgers", cols, vals, function(res) {
-        cb(res);
-      });
+    create: async (burgerName)=> {
+      return await orm.insertOne(burgerName)
     },
-    update: function(objColVals, condition, cb) {
-      orm.update("burgers", objColVals, condition, function(res) {
-        cb(res);
-      });
+    update: async (burgerId)=> {
+      return await orm.insertOne(burgerId)
     },
-    delete: function(condition, cb) {
-      orm.delete("burgers", condition, function(res) {
-        cb(res);
-      });
-    }
   };
 
-// Export the database functions for the controller (catsController.js).
 module.exports = burger;
