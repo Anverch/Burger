2759f6425f0b2e3db53c4d1490b680715b3eb419
Aleksandr Saiyan <anverch@gmail.com>
2020-06-14 15:09:29 -0500

    add error

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,14 +3,19 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", async (req, res)=> {
-    const data = await burger.all();
+    try {
+      const data = await burger.all();
     var hbsObject = {
       burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
+  } catch (err) {
+    console.error(err);
+    res.render("index", {burgers: [], error: err})
+  }
 });
-
+ 
 router.post("/api/burger", async (req, res)=> {
   console.log(req)
   const response = await burger.create(req.body.data.burgerName);
